28d4a2011a1b16630589a898154741f0c91fbcb5
Lukas Schöbel <56588215+lukaschoebel@users.noreply.github.com>
2020-04-16 08:28:41 +0200

    adding default test images

--- scripts/tesseract_ocr.py
+++ scripts/tesseract_ocr.py
@@ -4,24 +4,25 @@ import argparse
 import cv2
 import os
 
-def tesseract_ocr(image_name):
+def tesseract_ocr(image_name, preprocess="thresh"):
     """ Application of tesseract on image
     :param: string: image_name: Specifies name of the image
+    :param: string: preprocess: Specifies the preprocessing options [thresh, blur]
     """
     # load the example image and convert it to grayscale
     image = cv2.imread(image_name)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # check to see if we should apply thresholding to preprocess the
-    # image
-    if args["preprocess"] == "thresh":
+
+    # apply thresholding
+    if preprocess == "thresh":
         gray = cv2.threshold(gray, 0, 255,
             cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    # make a check to see if median blurring should be done to remove
-    # noise
-    elif args["preprocess"] == "blur":
+    # median blurring for noise removal
+    elif preprocess == "blur":
         gray = cv2.medianBlur(gray, 3)
-    # write the grayscale image to disk as a temporary file so we can
-    # apply OCR to it
+
+    # write the grayscale image to disk as a temporary 
+    # file so we can apply OCR to it
     filename = "{}.png".format(os.getpid())
     cv2.imwrite(filename, gray)
 
@@ -30,6 +31,7 @@ def tesseract_ocr(image_name):
     text = pytesseract.image_to_string(Image.open(filename))
     os.remove(filename)
     print(text)
+
     # show the output images
     cv2.imshow("Image", image)
     cv2.imshow("Output", gray)
@@ -41,9 +43,9 @@ if __name__ == "__main__":
     ap.add_argument("-i", "--image", required=True,
                     help="path to input image to be OCR'd")
     ap.add_argument("-p", "--preprocess", type=str, default="thresh",
-                    help="type of preprocessing to be done")
+                    help="Specifies the type of preprocessing that is conducted. Default is Threshholding.")
     args = vars(ap.parse_args())
 
-    tesseract_ocr(args["image"])
+    tesseract_ocr(args["image"], args["preprocess"])
 
     
